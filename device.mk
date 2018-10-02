4ed1d71485c271436700f4389086b134cefc6541
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    y2q: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -11,5 +11,9 @@ $(call inherit-product, device/samsung/sm8250-common/common.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/samsung/y2q/y2q-vendor.mk)
