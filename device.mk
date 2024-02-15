498d95ab5416edcd30e1f750bdb100be3c7d937f
DY3413 <milosix3413@gmail.com>
2024-02-15 17:14:41 +0900

    y2q: Set resolution to FHD+ * Set to 2400x1080 to enable high refresh rate. * Also define screen density to 450.

--- device.mk
+++ device.mk
@@ -7,5 +7,9 @@
 # Inherit from sm8250-common
 $(call inherit-product, device/samsung/sm8250-common/common.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/samsung/y2q/y2q-vendor.mk)
