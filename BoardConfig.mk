1a3396b58fb1b3955c9ac98561260ce144454b9a
DY3413 <milosix3413@gmail.com>
2024-02-23 16:28:09 +0900

    y2q: Build kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ DEVICE_PATH := device/samsung/y2q
 # Display
 TARGET_SCREEN_DENSITY := 450
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/kona_sec_y2q_defconfig
+
 # Inherit from the proprietary version
 include vendor/samsung/y2q/BoardConfigVendor.mk
